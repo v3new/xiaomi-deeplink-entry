4958518308d5d15b1d12307626658f4960f1c6ae
Sasha Verenuk <verenuk@gmail.com>
2025-04-11 02:21:48 +0300

    extract components into separate files

---
+++ data/services.js
@@ -0,0 +1,82 @@
+const SERVICES = {
+  music: [
+    {
+      id: 'yandex-music',
+      name: 'Yandex Music',
+      package: 'ru.yandex.music',
+      icon: 'https://play-lh.googleusercontent.com/mfSdQmzaqOdlWlygbCuJqNC0TNOSbjdXOH9BN9MVyuYYp5p6VD54KOmRmgPVv2sXlkc=s64-rw',
+    },
+    {
+      id: 'youtube-music',
+      name: 'YouTube Music',
+      package: 'com.google.android.apps.youtube.music',
+      icon: 'https://play-lh.googleusercontent.com/zD8UA5CRdiPzbvTwGKtzR4KjQpxqEK6X0tGDpzEaOo0xPEvG6HUiC_0qkpTfzpuMTqU=s64-rw',
+    },
+    {
+      id: 'apple-music',
+      name: 'Apple Music',
+      package: 'com.apple.android.music',
+      icon: 'https://play-lh.googleusercontent.com/mOkjjo5Rzcpk7BsHrsLWnqVadUK1FlLd2-UlQvYkLL4E9A0LpyODNIQinXPfUMjUrbE=s64-rw',
+    }
+  ],
+  navigation: [
+    {
+      id: 'yandex-navi',
+      name: 'Yandex Navi',
+      package: 'ru.yandex.yandexnavi',
+      icon: 'https://play-lh.googleusercontent.com/quo2QK2mxipWCZiBTM58nyENopQsvXpLz1wqLOhlBg8TxJ8tEH_T9buZVdRnSDa-vPY=s64-rw',
+    },
+    {
+      id: 'yandex-maps',
+      name: 'Yandex Maps',
+      package: 'ru.yandex.yandexmaps',
+      icon: 'https://play-lh.googleusercontent.com/DLCdDuCkVMI-vQhbNmPJU8cIDZulGHJxYGz_Cm9Mbrv6ssl9TW-RUMXfzczd9NKZj4w=s64-rw',
+    },
+    {
+      id: 'google-maps',
+      name: 'Google Maps',
+      package: 'com.google.android.apps.maps',
+      icon: 'https://play-lh.googleusercontent.com/Kf8WTct65hFJxBUDm5E-EpYsiDoLQiGGbnuyP6HBNax43YShXti9THPon1YKB6zPYpA=s64-rw',
+    }
+  ],
+  work: [
+    {
+      id: 'zoom',
+      name: 'Zoom',
+      package: 'us.zoom.videomeetings',
+      icon: 'https://play-lh.googleusercontent.com/yZsmiNjmji3ZoOuLthoVvptLB9cZ0vCmitcky4OUXNcEFV3IEQkrBD2uu5kuWRF5_ERA=s64-rw',
+    },
+    {
+      id: 'chatgpt',
+      name: 'ChatGPT',
+      package: 'com.openai.chatgpt',
+      icon: 'https://play-lh.googleusercontent.com/lmG9HlI0awHie0cyBieWXeNjpyXvHPwDBb8MNOVIyp0P8VEh95AiBHtUZSDVR3HLe3A=s64-rw',
+    },
+    {
+      id: 'deepseek',
+      name: 'DeepSeek',
+      package: 'com.deepseek.chat',
+      icon: 'https://play-lh.googleusercontent.com/d2zqBFBEymSZKaVg_dRo1gh3hBFn7_Kl9rO74xkDmnJeLgDW0MoJD3cUx0QzZN6jdsg=s64-rw',
+    }
+  ],
+  entertainment: [
+    {
+      id: 'kinopub',
+      name: 'Kinopub',
+      package: 'com.kinopub',
+      icon: 'https://img.iconpusher.com/com.kinopub/1.33.png?w=64&q=75',
+    },
+    {
+      id: 'kinopoisk',
+      name: 'Kinopoisk',
+      package: 'ru.kinopoisk',
+      icon: 'https://play-lh.googleusercontent.com/5czw6iycA8YhjI653GQdwnnmu8NNzEMXV32gZKoVCYZV6PQUAv_YV0uJ2PU1E-Jm9PE=s64-rw',
+    },
+    {
+      id: 'youtube',
+      name: 'YouTube',
+      package: 'com.google.android.youtube',
+      icon: 'https://play-lh.googleusercontent.com/6am0i3walYwNLc08QOOhRJttQENNGkhlKajXSERf3JnPVRQczIyxw2w3DxeMRTOSdsY=s64-rw',
+    }
+  ]
+};
